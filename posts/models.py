aea9969336536b16aaab2a4fad11b689e5d2cf8c
totallyglurbo <glurbin@glglgl.com>
2025-12-05 10:38:13 +0700

    started project and didn't delete .idea

---
+++ posts/models.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class ReallyUser(AbstractUser):
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True,
+                               verbose_name='Аватар', default='avatars/image.png')
+
+
+    class Meta(AbstractUser.Meta):
+        pass
